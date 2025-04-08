62bc31fe1d02b8f572a50e744f4db2b09751bc3e
Yaroslav Terekh <yarolslavterekh@gmail.com>
2025-04-08 14:39:13 +0300

    Add banner entity

---
+++ Reabilit.Domain/Entities/Banner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reabilit.Domain.Entities;
+
+public class Banner : BaseEntity
+{
+    public required string ImageName { get; set; }
+    public required string ImagePath { get; set; }
+    public required string Description { get; set; }
+}
---
+++ Reabilit.Domain/Migrations/20250408113636_AddBannerEntity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Reabilit.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBannerEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Banners",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ImageName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImagePath = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Banners", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Banners");
+        }
+    }
+}
