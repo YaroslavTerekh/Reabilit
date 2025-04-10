fb43878599c6c5a8eb964c6c3860da8a0c74ea22
Yaroslav Terekh <yarolslavterekh@gmail.com>
2025-04-10 18:08:00 +0300

    add my profile page

---
+++ Reabilit.UI/src/app/pages/my-profile-page/my-profile-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-my-profile-page',
+  imports: [RouterLink],
+  templateUrl: './my-profile-page.component.html',
+  styleUrl: './my-profile-page.component.scss'
+})
+export class MyProfilePageComponent {
+
+}
