c6dcf54bd64e43cdc7885bca2ac08ca12f7037a2
Yaroslav Terekh <yarolslavterekh@gmail.com>
2025-04-12 12:35:03 +0300

    added auth service

--- Reabilit.UI/src/app/services/authorization/auth.service.ts
+++ Reabilit.UI/src/app/services/authorization/auth.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@angular/core';
-import { ReplaySubject } from 'rxjs';
+import { Observable, ReplaySubject } from 'rxjs';
+import { config } from '../../config';
+import { LoginModel } from '../requestModels/LoginModel';
+import { AuthToken } from '../responseModels/AuthToken';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   public $showLoginModalSubject: ReplaySubject<boolean> = new ReplaySubject<boolean>();
+  private readonly baseUrl: string = config.apiUrl;
 
-  constructor() { }
+  constructor(
+    private readonly http: HttpClient
+  ) { }
+
+  public login(request: LoginModel): Observable<AuthToken> {
+    return this.http.post<AuthToken>(`${this.baseUrl}/Auth/login`, request);
+  }
 }
