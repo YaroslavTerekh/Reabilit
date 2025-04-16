efaa976060a029fc996ac156611d3652e133b088
Yaroslav Terekh <yarolslavterekh@gmail.com>
2025-04-16 16:04:44 +0300

    added attach doctor endpoint

--- Reabilit.API/Controllers/AdminController.cs
+++ Reabilit.API/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Reabilit.BL.Behaviours.Authentication.RegisterDoctor;
 using Reabilit.BL.Behaviours.Authentication.RegisterPatient;
+using Reabilit.BL.Behaviours.UserPatient.AttachToDoctor;
 using Reabilit.Domain.Constants;
 
 namespace Reabilit.API.Controllers;
@@ -30,6 +31,14 @@ public class AdminController : ControllerBase
     public async Task<IActionResult> RegisterDoctorAsync([FromBody] RegisterDoctorCommand command, CancellationToken cancellationToken = default)
     {
         await _sender.Send(command, cancellationToken);
+        return Ok();
+    }
+
+    [HttpPost("patient/attach-doctor")]
+    public async Task<IActionResult> AttachToDoctorAsync([FromBody] AttachToDoctorCommand command, CancellationToken cancellationToken = default)
+    {
+        await _sender.Send(command, cancellationToken);
+
         return Ok();
     }
 }
---
+++ Reabilit.BL/Behaviours/UserPatient/AttachToDoctor/AttachToDoctorCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reabilit.BL.Behaviours.UserPatient.AttachToDoctor;
+
+public class AttachToDoctorCommand : IRequest
+{
+    public Guid DoctorId { get; set; }
+    
+    public Guid PatientId { get; set; }
+}
