db7f157cebdff15b3d00e8c136b834f2648e5fd2
Yaroslav Terekh <yarolslavterekh@gmail.com>
2025-04-16 16:03:36 +0300

    added getCities endpoint

---
+++ Reabilit.BL/Behaviours/Content/Cities/GetAllCities/GetAllCitiesQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Reabilit.Domain.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reabilit.BL.Behaviours.Content.Cities.GetAllCities;
+
+public class GetAllCitiesQuery : IRequest<List<CityDTO>>
+{
+}
---
+++ Reabilit.BL/Behaviours/Content/Cities/GetAllCities/GetAllCitiesQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Reabilit.Domain.DbConnection;
+using Reabilit.Domain.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reabilit.BL.Behaviours.Content.Cities.GetAllCities;
+
+public class GetAllCitiesQueryHandler : IRequestHandler<GetAllCitiesQuery, List<CityDTO>>
+{
+    private readonly DataContext _context;
+
+    public GetAllCitiesQueryHandler(DataContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<CityDTO>> Handle(GetAllCitiesQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.Cities
+            .Select(c => new CityDTO {
+                Id = c.Id,
+                CityName = c.CityName
+            }).ToListAsync(cancellationToken);
+    }
+}
