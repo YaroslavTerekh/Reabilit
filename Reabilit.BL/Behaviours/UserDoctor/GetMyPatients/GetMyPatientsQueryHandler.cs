3ebcb2e71eea9fbe15b0325f6665b18ca54a6f91
Yaroslav Terekh <yarolslavterekh@gmail.com>
2025-04-22 17:39:30 +0300

    Added get my patients endpoint

---
+++ Reabilit.BL/Behaviours/UserDoctor/GetMyPatients/GetMyPatientsQueryHandler.cs
@@ -0,0 +1,83 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Reabilit.Domain.Constants;
+using Reabilit.Domain.DbConnection;
+using Reabilit.Domain.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reabilit.BL.Behaviours.UserDoctor.GetMyPatients;
+
+public class GetMyPatientsQueryHandler : IRequestHandler<GetMyPatientsQuery, List<PatientDTO>>
+{
+    private readonly DataContext _context;
+
+    public GetMyPatientsQueryHandler(DataContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<PatientDTO>> Handle(GetMyPatientsQuery request, CancellationToken cancellationToken)
+    {
+        var doctorId = await _context.Doctors
+            .Where(d => d.AppUserId == request.CurrentUserId)
+            .Select(d => d.Id)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if(doctorId == Guid.Empty)
+        {
+            throw new AuthException(StatusCodes.Status401Unauthorized, ErrorMessages.Unauthorized401);
+        }
+
+        return await _context.Patients
+            .Where(p => p.DoctorId == doctorId)
+            .Select(p => new PatientDTO
+            {
+                Id = p.Id,
+                AppUserId = p.AppUserId,
+                Age = p.AppUser!.Age,
+                City = new CityDTO
+                {
+                    Id = p.CityId,
+                    CityName = p.City!.CityName
+                },
+                Doctor = new DoctorDTO
+                {
+                    Age = p.Doctor!.AppUser!.Age,
+                    AppUserId = p.Doctor.AppUserId,
+                    Biography = p.Doctor.Biography,
+                    Degree = p.Doctor.Degree,
+                    ExperienceInYear = p.Doctor.ExperienceInYear,
+                    FirstName = p.Doctor.AppUser.FirstName,
+                    LastName = p.Doctor.AppUser.LastName,
+                    Id = p.Doctor.Id,
+                    PhoneNumber = p.Doctor!.AppUser!.PhoneNumber!,
+                    DoctorClass = new DoctorClassDTO
+                    {
+                        Id = p.Doctor.DoctorClass!.Id,
+                        ClassName = p.Doctor.DoctorClass.ClassName
+                    },
+                    DoctorClassId = p.Doctor.DoctorClassId
+                },
+                CityId = p.CityId,
+                FirstName = p.AppUser.FirstName,
+                LastName = p.AppUser.LastName,
+                PhoneNumber = p.AppUser!.PhoneNumber!,
+                DoctorId = p.DoctorId,
+                IsActive = p.IsActive,
+                Analyzes = p.Analyzes.Select(pa => new AnalyzeDTO
+                {
+                    Title = pa.Title,
+                    IsNormal = pa.IsNormal,
+                    Unit = pa.Unit,
+                    Value = pa.Value,
+                    IconPath = pa.IconPath
+                }).ToList()
+            })
+            .ToListAsync(cancellationToken);
+    }
+}
--- Reabilit.Domain/DTOs/PatientDTO.cs
+++ Reabilit.Domain/DTOs/PatientDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Reabilit.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,13 @@ namespace Reabilit.Domain.DTOs;
 
 public class PatientDTO : UserDTO
 {
+    public List<AnalyzeDTO> Analyzes { get; set; } = new();
+
     public Guid? DoctorId { get; set; }
     public DoctorDTO? Doctor { get; set; }
 
     public Guid CityId { get; set; }
     public CityDTO City { get; set; }
+
+    public bool IsActive { get; set; }
 }
