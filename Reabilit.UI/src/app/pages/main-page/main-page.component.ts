01a9d8db0471d28c0a92893be0864098dc2ff293
Yaroslav Terekh <yarolslavterekh@gmail.com>
2025-04-28 18:32:43 +0300

    Divided main page logic

--- Reabilit.UI/src/app/pages/main-page/main-page.component.ts
+++ Reabilit.UI/src/app/pages/main-page/main-page.component.ts
@@ -7,6 +7,11 @@ import { BannerDTO } from '../../services/responseModels/BannerDTO';
 import { CommonModule } from '@angular/common';
 import { AnalyzeDTO } from '../../services/responseModels/AnalyzeDTO';
 import { PatientService } from '../../services/patient/patient.service';
+import { AuthService } from '../../services/authorization/auth.service';
+import { DoctorService } from '../../services/doctor/doctor.service';
+import { ProcedureEventDTO } from '../../services/responseModels/ProcedureEventDTO';
+import { EventCardComponent } from '../../common-ui/event-card/event-card.component';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-main-page',
@@ -19,26 +24,44 @@ import { PatientService } from '../../services/patient/patient.service';
 export class MainPageComponent implements OnInit, OnDestroy {
   banners: BannerDTO[] = [];
   patientAnalyzes: AnalyzeDTO[] = [];
+  myEvents$!: Observable<ProcedureEventDTO[]>;
   currentBannerIndex: number = 0;
   interval: any;
+  currentRole!: string;
 
   constructor(
     private readonly contentService: ContentService,
-    private readonly patientService: PatientService
-  ) {}
+    private readonly authService: AuthService,
+    private readonly patientService: PatientService,
+    private readonly doctorService: DoctorService
+  ) { }
 
   ngOnInit(): void {
+    this.authService.$currentRole.subscribe({
+      next: res => {
+        this.currentRole = res;
+
+
+        if (this.currentRole == "Patient") {
+          this.patientService.GetMyAnalyzes()
+            .subscribe({
+              next: res => {
+                this.patientAnalyzes = res;
+              }
+            })
+        }
+
+        if (this.currentRole == "Doctor") {
+          this.loadEvents();
+        }
+      }
+    })
+
+
     this.contentService.getBanners().subscribe({
       next: (res) => {
         this.banners = res;
         this.startBannerRotation();
-
-        this.patientService.GetMyAnalyzes()
-          .subscribe({
-            next: res => {
-              this.patientAnalyzes = res;
-            }
-          })
       },
     });
   }
@@ -49,14 +72,33 @@ export class MainPageComponent implements OnInit, OnDestroy {
     }
   }
 
+  loadEvents(): void {
+    this.myEvents$ = this.doctorService.GetMyTodaysEvents();
+  }
+
   startBannerRotation() {
     this.interval = setInterval(() => {
       this.currentBannerIndex =
         (this.currentBannerIndex + 1) % this.banners.length;
-    }, 10000); 
+    }, 10000);
   }
 
   get currentBanner(): BannerDTO {
     return this.banners[this.currentBannerIndex];
+  }
+
+  formatUADateTime(isoString: string): string {
+    const date = new Date(isoString);
+
+    const day = date.getDate();
+    const monthNames = [
+      'січня', 'лютого', 'березня', 'квітня', 'травня', 'червня',
+      'липня', 'серпня', 'вересня', 'жовтня', 'листопада', 'грудня'
+    ];
+    const month = monthNames[date.getMonth()];
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+
+    return `${day} ${month} ${hours}:${minutes}`;
   }
 }
